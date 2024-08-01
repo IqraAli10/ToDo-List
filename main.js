0f5c08091d35a99bac29ed0adefabce19097d075
Iqra Ali <aiqra9786@gmail.com>
2024-08-01 18:38:39 +0500

    Iqra Ali

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addtask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what do you want to add in your Todo list?"
+        },
+        {
+            name: "Addmore",
+            type: "confirm",
+            message: "Do you want to add more in your Todo list?",
+            default: false
+        }
+    ]);
+    todos.push(addtask.todo);
+    condition = addtask.Addmore;
+    console.log(todos);
+}
